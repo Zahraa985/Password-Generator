01878a859f5cfd911a15f3023544389c865999b2
ZAHRAA A <ZAA19850@GMAIL.COM>
2020-04-18 18:03:41 -0700

    set user case var commit

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Assignment Code
+var specialCharacter = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
